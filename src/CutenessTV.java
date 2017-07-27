d7715c8bf6eb06fe491d5b86379b7e00a7efaae7
Lucas Baizer <titaniumsapphire32@gmail.com>
2017-07-27 11:55:16 -0700

    cuteness

--- src/CutenessTV.java
+++ src/CutenessTV.java
@@ -1,19 +1,48 @@
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.net.URI;
 
-public class CutenessTV {
+import javax.swing.JButton;
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class CutenessTV implements ActionListener {
+	JButton button1=new JButton("  Cute Ducks  ");
+	JButton button2=new JButton("  Cute Frogs  ");
+	JButton button3=new JButton("  Fluffy Unicorns  ");
 	
 	public static void main(String[] args) {
-		showDucks();
+		new CutenessTV().makeButtons();
 	}
 	
 	
+	public void makeButtons(){
+		JFrame frame= new JFrame();
+		frame.setVisible(true);
+		frame.setTitle("CutenessTV");
+		frame.setSize(400, 200);
+		
+		JPanel panel=new JPanel();
+		frame.add(panel);
+		
+		panel.add(button1);
+		panel.add(button2);
+		panel.add(button3);
+		frame.pack();
+		
+		button1.addActionListener(this);
+		button2.addActionListener(this);
+		button3.addActionListener(this);
+		
+		
+	}
 	
-	
-	static void showDucks() {
-		playVideo("https://youtu.be/LGrpsZ7BsQA");
+	 void showDucks() {
+		playVideo("https://youtube.com/watch?v=LGrpsZ7BsQA");
 	}
 
 	void showFrog() {
-		playVideo("https://youtu.be/cBkWhkAZ9ds");
+		playVideo("https://www.youtube.com/watch?v=cBkWhkAZ9ds&feature=youtu.be");
 	}
 	
 	void showFluffyUnicorns() {
@@ -22,10 +51,25 @@ public class CutenessTV {
 
 	void playVideo(String videoID) {
 		try {
-	URI uri = new URI("https://www.youtube.com/v/" + videoID + "?autoplay=1");
+	URI uri = new URI(videoID);
 			java.awt.Desktop.getDesktop().browse(uri);
 		} catch (Exception e) {
 			e.printStackTrace();
+		}
+	}
+
+
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		if(e.getSource() == button1){
+			showDucks();
+		}
+		else if(e.getSource()==button2){
+			showFrog();
+		}
+		else{
+			showFluffyUnicorns();
 		}
 	}
 
