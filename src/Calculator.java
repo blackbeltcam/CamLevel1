4e2a12efd8f5d32ce29ecb92dbb7eb042c1e350c
blackbeltcam <league@WTS20.attlocal.net>
2017-09-29 17:00:10 -0700

    Calculator(ip) Level1 Practice Test

--- src/Calculator.java
+++ src/Calculator.java
@@ -1,4 +1,6 @@
 import java.awt.BorderLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.JButton;
 import javax.swing.JFrame;
@@ -6,12 +8,16 @@ import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JTextField;
 
-public class Calculator extends JFrame {
+public class Calculator extends JFrame implements ActionListener {
 	JButton b1=new JButton("+");
 	JButton b2=new JButton("-");
 	JButton b3=new JButton("*");
 	JButton b4=new JButton("/");
 	
+	JTextField tf1 = new JTextField(10);
+	JTextField tf2 = new JTextField(10);
+	
+	JLabel l1=new JLabel("Answer:");
 	
 public static void main(String[] args) {
 	
@@ -25,6 +31,8 @@ public static void main(String[] args) {
 	setVisible(true);
 	setSize(500, 500);
 	
+	
+	
 	JPanel p1 = new JPanel();
 	JPanel p2 = new JPanel();
 	JPanel p3 = new JPanel();
@@ -33,8 +41,7 @@ public static void main(String[] args) {
 	add(p2, BorderLayout.CENTER);
 	add(p3, BorderLayout.SOUTH);
 	
-	JTextField tf1 = new JTextField(10);
-	JTextField tf2 = new JTextField(10);
+	
 	p1.add(tf1);
 	p1.add(tf2);
 	
@@ -43,11 +50,30 @@ public static void main(String[] args) {
 	p2.add(b3);
 	p2.add(b4);
 	
-	JLabel l1=new JLabel("Answer:");
+	b1.addActionListener(this);
+	b2.addActionListener(this);
+	b3.addActionListener(this);
+	b4.addActionListener(this);
+	
+	
 	p3.add(l1);
+	
+	pack();
 	
 		
 }
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		// TODO Auto-generated method stub
+		int n1=Integer.parseInt(tf1.getText());
+		int n2=Integer.parseInt(tf2.getText());
+		if (e.getSource()==b1) {
+			int answer = n1+n2;
+			l1.setText(String.valueOf(answer));
+		}
+		
+		
+	}
 
 
 
