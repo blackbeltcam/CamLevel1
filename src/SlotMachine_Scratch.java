942f4fc73db67f39f43116b8994a11cf97e0ea80
varnit05 <league@WTS20.attlocal.net>
2017-11-01 19:57:57 -0700

    SlotMachine_Scratch (ip)

---
+++ src/SlotMachine_Scratch.java
@@ -0,0 +1,29 @@
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+
+public class SlotMachine_Scratch extends JFrame {
+public static void main(String[] args) {
+	new SlotMachine_Scratch();
+}
+
+public SlotMachine_Scratch () {
+	JFrame frame = new JFrame();
+	setSize(500, 500);
+	setName("SPIN TO WIN!!");
+	setVisible(true);
+	setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+	
+	JPanel panel = new JPanel ();
+	
+	
+}
+
+
+
+
+
+
+
+
+
+}
